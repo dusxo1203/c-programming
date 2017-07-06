067d49cc772c5334b2888884d5f22ad403762b22
dusxo1203 <dusxo1203@gmail.com>
2017-07-06 21:21:45 +0900

    hi
    hi

---
+++ code/tae1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("hellow\n");
+
+	int a = 19;
+	printf("my age is %d\n", 19);
+	
+	float b = 19.19;
+	printf("real age is %f\n", b);
+	
+	char c = 'C';
+
+	float sum = a + b; // ����
+	printf("my age add real age is %f\n", sum);
+
+	float min = a - b; // ����
+	printf("something is %f\n", a - b);
+
+	float mult = a * b; // ����
+	printf("multi is %f\n", mult);
+	
+	float divi = a / b; // ������
+	printf("divide is %f\n", divi);
+}
