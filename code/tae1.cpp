46524a4c5ea227debc094078c9936632c8ee0865
dusxo1203 <dusxo1203@gmail.com>
2017-07-12 19:54:16 +0900

    hi
    hi

--- code/tae1.cpp
+++ code/tae1.cpp
@@ -11,16 +11,17 @@ int main()
 	printf("real age is %f\n", b);
 	
 	char c = 'C';
+	printf("my grade is %c\n", c);
 
-	float sum = a + b; // ï¿½ï¿½ï¿½ï¿½
+	float sum = a + b; // µ¡¼À
 	printf("my age add real age is %f\n", sum);
 
-	float min = a - b; // ï¿½ï¿½ï¿½ï¿½
+	float min = a - b; // »¬»ù
 	printf("something is %f\n", a - b);
 
-	float mult = a * b; // ï¿½ï¿½ï¿½ï¿½
+	float mult = a * b; // °ö¼À
 	printf("multi is %f\n", mult);
 	
-	float divi = a / b; // ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
+	float divi = a / b; // ³ª´°¼À
 	printf("divide is %f\n", divi);
 }
